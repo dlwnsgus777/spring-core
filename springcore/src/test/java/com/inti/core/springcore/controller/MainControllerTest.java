dd0766aeb89a253fcb349f4837bc2974e92e75ed
Ted <dlwnsgus777@naver.com>
2023-06-01 11:15:01 +0900

    feat: @RestController 를 붙였을 때 Text로 내려가는 내부 동작 디버깅 연습
    feat: @RestController 를 붙였을 때 Text로 내려가는 내부 동작 디버깅 연습

--- springcore/src/test/java/com/inti/core/springcore/controller/MainControllerTest.java
+++ springcore/src/test/java/com/inti/core/springcore/controller/MainControllerTest.java
@@ -2,29 +2,27 @@ package com.inti.core.springcore.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.verify;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@WebMvcTest(MainController.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class MainControllerTest {
 
+   @LocalServerPort
+   int port;
+
    @Autowired
-   MockMvc mockMvc;
+   private TestRestTemplate testRestTemplate;
 
    @Test
    public void main_controller_test() throws Exception {
-      mockMvc.perform(get("/"))
-            .andExpect(status().isOk())
-            .andExpect(content().string(containsString("test")))
-            .andDo(print());
+      ResponseEntity<String> response = testRestTemplate.getForEntity("http://localhost:" + port, String.class);
+
+      assertThat(response.getBody()).isEqualTo("test");
    }
 
 }
